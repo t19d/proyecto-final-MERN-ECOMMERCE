52d51808305c6d1ab64c6fae3a47bf99cd0656fe
David <davidtojo99@gmail.com>
2021-01-30 12:21:37 +0100

    Añadido footer.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -95,12 +95,90 @@ function App() {
           </div>
         </div>
       </header>
-      
+
       <div id="contenidoPrincipal">
         <Route path="/" exact={true} component={HomeScreen} />
         <Route path="/productos/:id" component={ProductoScreen} />
         <Route path="/equipaciones" component={EquipacionesScreen} />
       </div>
+
+
+      <footer className="page-footer font-small pt-5">
+        <hr />
+        <div className="container text-center text-md-left">
+          <div className="row">
+            <div className="col-md-4 mx-auto">
+              <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Footer Content</h5>
+              <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+              consectetur
+  adipisicing elit.</p>
+            </div>
+            <hr className="clearfix w-100 d-md-none" />
+            <div className="col-md-2 mx-auto">
+              <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#!">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!">Link 2</a>
+                </li>
+              </ul>
+            </div>
+            <hr className="clearfix w-100 d-md-none" />
+            <div className="col-md-2 mx-auto">
+              <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#!">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!">Link 2</a>
+                </li>
+              </ul>
+            </div>
+            <hr className="clearfix w-100 d-md-none" />
+            <div className="col-md-2 mx-auto">
+              <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#!">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!">Link 2</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <ul className="list-unstyled list-inline text-center">
+          <li className="list-inline-item">
+            <a className="btn-floating btn-fb mx-1">
+              <i className="fab fa-facebook-f"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-tw mx-1">
+              <i className="fab fa-twitter"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-gplus mx-1">
+              <i className="fab fa-google-plus-g"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-li mx-1">
+              <i className="fab fa-linkedin-in"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-dribbble mx-1">
+              <i className="fab fa-dribbble"> </i>
+            </a>
+          </li>
+        </ul>
+      </footer>
 
     </BrowserRouter>
   );
