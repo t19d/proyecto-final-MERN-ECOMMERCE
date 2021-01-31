980b382a550fc1958bc18af0c260c3779cf51fcc
David <davidtojo99@gmail.com>
2021-01-31 12:55:48 +0100

    Cambio a por Link.

--- frontend/src/screens/ProductoScreen.js
+++ frontend/src/screens/ProductoScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import data from '../data';
 
 function ProductoScreen(props) {
@@ -9,13 +10,19 @@ function ProductoScreen(props) {
             <div className="container">
                 <nav aria-label="breadcrumb">
                     <ol className="breadcrumb">
-                        <li className="breadcrumb-item"><a href="/">
-                            <svg width="2em" height="1em" viewBox="0 0 16 16" className="bi bi-house-door-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z" />
-                                <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
-                            </svg>
-                        </a></li>
-                        <li className="breadcrumb-item"><a href="/equipaciones">Equipaciones</a></li>
+                        <li className="breadcrumb-item">
+                            <Link to="/">
+                                <svg width="2em" height="1em" viewBox="0 0 16 16" className="bi bi-house-door-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z" />
+                                    <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                                </svg>
+                            </Link>
+                        </li>
+                        <li className="breadcrumb-item">
+                            <Link to="/equipaciones">
+                                <span href="/equipaciones">Equipaciones</span>
+                            </Link>
+                        </li>
                         <li className="breadcrumb-item active" aria-current="page">{producto.nombre}</li>
                     </ol>
                 </nav>
