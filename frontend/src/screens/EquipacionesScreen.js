b89d13397f5b8af54ccbb0cdb0228c9b0a1f58c7
David <davidtojo99@gmail.com>
2021-01-31 13:02:17 +0100

    Reordenados elementos.

--- frontend/src/screens/EquipacionesScreen.js
+++ frontend/src/screens/EquipacionesScreen.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { hacerListaProductos } from '../actions/productoAcciones'
 
 function EquipacionesScreen(props) {
-    
+
     const listaProductos = useSelector(state => state.listaProductos);
     const { productos, loading, error } = listaProductos;
     const dispatch = useDispatch();
@@ -18,27 +18,31 @@ function EquipacionesScreen(props) {
     }, []);
 
     return (
-        loading ? <div>Loading...</div> :
-        error ? <div>{error}</div> :
         <section>
             <img src="/images/recursos_web/cabecera_camiseta_blanca_grande.png" width="100%" alt="Cabecera camiseta blanca" />
             <div className="container">
-                <div className="row listaProductos">
-                    {
-                        productos.map(
-                            producto =>
-                                (<Link to={'/productos/' + producto._id} className="col-sm itemProductos" key={producto._id}>
-                                    <div className="card text-center">
-                                        <div className="card-body">
-                                            <img src={producto.miniatura} className="card-img imagenItem" alt={producto.imgDescripcion} />
-                                            <h5 className="card-title">{producto.nombre}</h5>
-                                            <h5 className="card-title precioItem">{producto.precio} €</h5>
-                                        </div>
-                                    </div>
-                                </Link>)
-                        )
-                    }
-                </div>
+                {
+                    loading ? <div>Cargando...</div> :
+                        error ? <div>{error}</div> :
+                            (
+                                <div className="row listaProductos">
+                                    {
+                                        productos.map(
+                                            producto =>
+                                            (<Link to={'/productos/' + producto._id} className="col-sm itemProductos" key={producto._id}>
+                                                <div className="card text-center">
+                                                    <div className="card-body">
+                                                        <img src={producto.miniatura} className="card-img imagenItem" alt={producto.imgDescripcion} />
+                                                        <h5 className="card-title">{producto.nombre}</h5>
+                                                        <h5 className="card-title precioItem">{producto.precio} €</h5>
+                                                    </div>
+                                                </div>
+                                            </Link>)
+                                        )
+                                    }
+                                </div>
+                            )
+                }
             </div>
         </section>);
 }
