bb015e4fb789b36e13245b83816d1e7f79f37a6f
David <davidtojo99@gmail.com>
2021-01-31 13:17:08 +0100

    Estados de petición de detalles.

--- frontend/src/reducers/productoReducers.js
+++ frontend/src/reducers/productoReducers.js
@@ -1,4 +1,4 @@
-import { LISTA_PRODUCTOS_FAIL, LISTA_PRODUCTOS_REQUEST, LISTA_PRODUCTOS_SUCCESS } from "../constants/productoConstantes";
+import { LISTA_PRODUCTOS_FAIL, LISTA_PRODUCTOS_REQUEST, LISTA_PRODUCTOS_SUCCESS, PRODUCTO_DETALLES_FAIL, PRODUCTO_DETALLES_REQUEST, PRODUCTO_DETALLES_SUCCESS } from "../constants/productoConstantes";
 
 function listaProductosReducer(state = { productos: [] }, action) {
     switch (action.type) {
@@ -13,4 +13,17 @@ function listaProductosReducer(state = { productos: [] }, action) {
     }
 }
 
-export { listaProductosReducer }
+function productoDetallesReducer(state = { producto: {} }, action) {
+    switch (action.type) {
+        case PRODUCTO_DETALLES_REQUEST:
+            return { loading: true };
+        case PRODUCTO_DETALLES_SUCCESS:
+            return { loading: false, producto: action.payload };
+        case PRODUCTO_DETALLES_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+
+export { listaProductosReducer, productoDetallesReducer }
