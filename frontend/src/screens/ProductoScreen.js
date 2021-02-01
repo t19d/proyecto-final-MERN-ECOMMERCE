918be4bce7e9afcaee07985c945cd40ba2da8982
David <davidtojo99@gmail.com>
2021-02-01 18:29:37 +0100

    Manejar añadir al carrito y la cantidad

--- frontend/src/screens/ProductoScreen.js
+++ frontend/src/screens/ProductoScreen.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { hacerProductoDetalles } from '../actions/productoAcciones';
 
 function ProductoScreen(props) {
+    const [cantidad, setCantidad] = useState(1);
     const productoDetalles = useSelector(state => state.productoDetalles);
     const { producto, loading, error } = productoDetalles;
     const dispatch = useDispatch();
@@ -13,7 +14,18 @@ function ProductoScreen(props) {
         return () => {
             //
         };
-    }, [])
+    }, []);
+
+    {/*
+        //TODO:
+            - Hacer lo mismo con las tallas
+            - Dependiendo de la talla poner la cantidad relativa a la talla
+            - Ocultar cantidad o seleccionar una talla de manera predeterminada al iniciar
+    */}
+
+    const handleAnhadirCarrito = () => {
+        props.history.push("/carrito/" + props.match.params.id + "?cantidad=" + cantidad);
+    }
 
     return (
         loading ? <div>Cargando...</div> :
@@ -78,24 +90,39 @@ function ProductoScreen(props) {
                                 XXL</label>
                                     </div>
                                 </div>
-                                <div className="cantidadProducto">
-                                    <span className="">Cantidad:</span>
-                                    <select>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
+                                {producto.cantidadStokL > 0 && <div className="cantidadProducto">
+                                    <span>Cantidad:</span>
+                                    <select value={cantidad} onChange={(event) => setCantidad(event.target.value)}>
+                                        {[...Array(producto.cantidadStokL).keys()].map(
+                                            (x) => (
+                                                <option key={x + 1} value={x + 1}>
+                                                    {x + 1}
+                                                </option>
+                                            )
+                                        )}
                                     </select>
                                 </div>
-                                <button type="button" className="btn btn-primary btn-lg btn-block botonAnhadirCarrito">
-                                    <span>
-                                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-cart-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path fillRule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                                            <path fillRule="evenodd" d="M8.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 .5-.5z" />
-                                        </svg>
+                                }
+                                {producto.cantidadStokL > 0 ?
+                                    <button onClick={handleAnhadirCarrito} type="button" className="btn btn-primary btn-lg btn-block botonAnhadirCarrito">
+                                        <span>
+                                            <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-cart-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path fillRule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                                <path fillRule="evenodd" d="M8.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 .5-.5z" />
+                                            </svg>
                                         Añadir al carrito
                                     </span>
-                                </button>
+                                    </button>
+                                    :
+                                    <div className="sinStockAvisoDetalles">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height=".75em" fill="currentColor" class="bi bi-emoji-frown" viewBox="0 0 16 18">
+                                            <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                            <path fill-rule="evenodd" d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683z" />
+                                            <path d="M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z" />
+                                        </svg>
+                                        Sin stock
+                                        </div>
+                                }
                             </div >
                         </div >
                         <div className="descripcionProducto">
