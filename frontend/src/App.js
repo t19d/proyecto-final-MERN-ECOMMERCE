d8adbb10d0fe8085357cdd89e18001a450d0723d
David <davidtojo99@gmail.com>
2021-02-01 18:31:04 +0100

    Añadida escena Carrito.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import EquipacionesScreen from './screens/EquipacionesScreen';
 import ProductoScreen from './screens/ProductoScreen';
+import CarritoScreen from './screens/CarritoScreen';
 
 function App() {
   return (
@@ -88,7 +89,7 @@ function App() {
                       <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
                         role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         AFICIÓN
-                                    </a>
+                      </a>
                       <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         {/*<a className="dropdown-item" href="/bufandas">BUFANDAS</a>*/}
                         <Link to="/bufandas" className="dropdown-item">
@@ -106,7 +107,15 @@ function App() {
                         <span>OUTLET</span>
                       </Link>
                     </li>
-
+                    <li className="nav-item">
+                      <Link to="/carrito" className="nav-link">
+                        <span>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                          </svg>
+                        </span>
+                      </Link>
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -117,6 +126,7 @@ function App() {
 
       <div id="contenidoPrincipal">
         <Route path="/" exact={true} component={HomeScreen} />
+        <Route path="/carrito/:id?" component={CarritoScreen} />
         <Route path="/productos/:id" component={ProductoScreen} />
         <Route path="/equipaciones" component={EquipacionesScreen} />
       </div>
