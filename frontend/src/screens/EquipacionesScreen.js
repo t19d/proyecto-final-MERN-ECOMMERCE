9e55d0a5738c2c4ddec5c53b4c6d5b4dcd28d65f
David <davidtojo99@gmail.com>
2021-02-02 20:26:47 +0100

    Eliminados imports no usados.

--- frontend/src/screens/EquipacionesScreen.js
+++ frontend/src/screens/EquipacionesScreen.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { hacerListaProductos } from '../actions/productoAcciones'
 
