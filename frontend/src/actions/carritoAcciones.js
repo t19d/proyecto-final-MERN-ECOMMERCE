166bf2703517d0636233217636a26eeab55acb93
David <davidtojo99@gmail.com>
2021-02-02 20:29:19 +0100

    Uso de Cookies implementado.

--- frontend/src/actions/carritoAcciones.js
+++ frontend/src/actions/carritoAcciones.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+import Cookie from "js-cookie";
 import { ANHADIR_ITEM_AL_CARRITO, ELIMINAR_ITEM_DEL_CARRITO } from "../constants/carritoConstantes";
 
-const anhadirAlCarrito = (productoId, cantidad) => async (dispatch) => {
+const anhadirAlCarrito = (productoId, cantidad) => async (dispatch, getState) => {
     try {
         const { data } = await axios.get("/api/productos/" + productoId);
         dispatch({
@@ -15,12 +16,20 @@ const anhadirAlCarrito = (productoId, cantidad) => async (dispatch) => {
                 cantidad
             }
         });
+        /* COOKIES */
+        const { carrito: { carritoItems } } = getState();
+        Cookie.set("carritoItems", JSON.stringify(carritoItems));
+
     } catch (error) {
     }
 }
 
-const eliminarDelCarrito = (productoId) => (dispatch) => {
+const eliminarDelCarrito = (productoId) => (dispatch, getState) => {
     dispatch({ type: ELIMINAR_ITEM_DEL_CARRITO, payload: productoId });
+
+    /* COOKIES */
+    const { carrito: { carritoItems } } = getState();
+    Cookie.set("carritoItems", JSON.stringify(carritoItems));
 }
 
 export { anhadirAlCarrito, eliminarDelCarrito }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,19 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from "redux-thunk";
+import Cookie from "js-cookie";
 import { carritoReducer } from './reducers/carritoReducers';
 import { listaProductosReducer, productoDetallesReducer } from './reducers/productoReducers';
 
-const initialState = {};
+const carritoItems = Cookie.getJSON("carritoItems") || [];
+
+const initialState = { carrito: { carritoItems } };
 const reducer = combineReducers({
     listaProductos: listaProductosReducer,
     productoDetalles: productoDetallesReducer,
     carrito: carritoReducer
 });
 
-{/* Usar herramienta de Chrome de Redux. */}
+{/* Usar herramienta de Chrome de Redux. */ }
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
