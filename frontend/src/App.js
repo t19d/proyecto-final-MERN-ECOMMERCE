34a87b20abda30b745dc919e25cd88ff085313af
David <davidtojo99@gmail.com>
2021-02-06 13:09:36 +0100

    Nombre en inicio de sesión

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,13 @@ import EquipacionesScreen from './screens/EquipacionesScreen';
 import ProductoScreen from './screens/ProductoScreen';
 import CarritoScreen from './screens/CarritoScreen';
 import InicioSesionScreen from './screens/InicioSesionScreen';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const usuarioInicioSesion = useSelector(state => state.usuarioInicioSesion);
+  const { usuarioInfo } = usuarioInicioSesion;
+
   return (
     <BrowserRouter>
       <header>
@@ -37,9 +42,14 @@ function App() {
                       <path fillRule="evenodd"
                         d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z" />
                     </svg>
-                    <Link to="/login" className="navbar-link text-white">
-                      <span> Iniciar sesión / Registrarse</span>
-                    </Link>
+                    {
+                      usuarioInfo ?
+                        <Link to="/perfil" className="navbar-link text-white">{usuarioInfo.nombre}</Link>
+                        :
+                        <Link to="/login" className="navbar-link text-white">
+                          <span>Iniciar sesión / Registrarse</span>
+                        </Link>
+                    }
                   </div>
                   <form className="form-inline">
                     <input className="form-control mr-sm-2" type="search" placeholder="Buscar..."
@@ -113,8 +123,8 @@ function App() {
                     <li className="nav-item">
                       <Link to="/carrito" className="nav-link">
                         <span>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
                           </svg>
                         </span>
                       </Link>
