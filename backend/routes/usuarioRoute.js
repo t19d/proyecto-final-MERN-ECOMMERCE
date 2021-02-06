b1e755fe9759cba0de3754efd0ddf25627875518
David <davidtojo99@gmail.com>
2021-02-06 13:14:47 +0100

    Registrar usuarios implementado.

--- backend/routes/usuarioRoute.js
+++ backend/routes/usuarioRoute.js
@@ -23,6 +23,26 @@ router.post('/iniciosesion', async (req, res) => {
     }
 });
 
+router.post('/registro', async (req, res) => {
+    const usuario = new Usuario({
+        nombre: req.body.nombre,
+        email: req.body.email,
+        password: req.body.password
+    });
+    const newUsuario = await usuario.save();
+    if (newUsuario) {
+        res.send({
+            _id: newUsuario.id,
+            nombre: newUsuario.nombre,
+            email: newUsuario.email,
+            isAdmin: newUsuario.isAdmin,
+            token: getToken(newUsuario)
+        });
+    } else {
+        res.status(401).send({msg: 'Datos de usuario no válidos.'});
+    }
+});
+
 router.get("/crearadmin", async (req, res) => {
     try {
         const usuario = new Usuario({
