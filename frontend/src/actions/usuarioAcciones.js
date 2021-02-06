9b82e22c59f799d9d55e120c5e135821b29a1e08
David <davidtojo99@gmail.com>
2021-02-06 13:18:43 +0100

    Añadido registrar

--- frontend/src/actions/usuarioAcciones.js
+++ frontend/src/actions/usuarioAcciones.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import Cookie from 'js-cookie';
-import { USUARIO_INICIOSESION_FAIL, USUARIO_INICIOSESION_REQUEST, USUARIO_INICIOSESION_SUCCESS } from '../constants/usuarioConstantes';
+import { USUARIO_INICIOSESION_FAIL, USUARIO_INICIOSESION_REQUEST, USUARIO_INICIOSESION_SUCCESS, USUARIO_REGISTRO_FAIL, USUARIO_REGISTRO_REQUEST, USUARIO_REGISTRO_SUCCESS } from '../constants/usuarioConstantes';
 
 const iniciarSesion = (email, password) => async (dispatch) => {
     dispatch({ type: USUARIO_INICIOSESION_REQUEST, payload: { email, password } });
@@ -13,4 +13,15 @@ const iniciarSesion = (email, password) => async (dispatch) => {
     }
 }
 
-export { iniciarSesion }
+const registrar = (nombre, email, password) => async (dispatch) => {
+    dispatch({ type: USUARIO_REGISTRO_REQUEST, payload: { nombre, email, password } });
+    try {
+        const { data } = await axios.post("/api/usuarios/registro", { nombre, email, password });
+        dispatch({ type: USUARIO_REGISTRO_SUCCESS, payload: data });
+        Cookie.set('userInfo', JSON.stringify(data));
+    } catch (error) {
+        dispatch({ type: USUARIO_REGISTRO_FAIL, payload: error.message });
+    }
+}
+
+export { iniciarSesion, registrar }
