c8aa1a1f69b1fa19f361f354918a18c4b02bffad
David <davidtojo99@gmail.com>
2021-02-06 13:28:37 +0100

    Tokens de admin y de auth

--- backend/util.js
+++ backend/util.js
@@ -12,4 +12,29 @@ const getToken = (usuario) => {
     });
 }
 
-export { getToken }
+const isAuth = (req, rest, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        const onlyToken = token.slice(7, token.length);
+        jwt.verify(onlyToken, config.JWT_SECRET, (error, decode) => {
+            if (error) {
+                return rest.status(401).send({ msg: 'Token inválido.' });
+            } else {
+                req.usuario = token;
+                next();
+                return
+            }
+        });
+    }
+    return rest.status.send({ msg: 'No se ha proporcionado el token.' });
+}
+
+const isAdmin = (req, rest, next) => {
+    if (req.user && req.user.isAdmin) {
+        return next();
+    } else {
+        return rest.status(401).send({ msg: 'Admin token no es válido' });
+    }
+}
+
+export { getToken, isAuth, isAdmin }
