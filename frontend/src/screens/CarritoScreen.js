e32aa8c8855ee6f5cdf4bdf1b9280af56818877d
David <davidtojo99@gmail.com>
2021-02-06 13:22:30 +0100

    Ruta actualizada.

--- frontend/src/screens/CarritoScreen.js
+++ frontend/src/screens/CarritoScreen.js
@@ -24,7 +24,7 @@ function CarritoScreen(props) {
     }, []);
 
     const checkoutHandler = () => {
-        props.history.push("/login?redirect=shipping");
+        props.history.push("/iniciosesion?redirect=shipping");
     }
 
     return (
