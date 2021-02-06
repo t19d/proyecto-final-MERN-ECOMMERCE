2f0130662da59dc2af1e9e25bab3b80d11f35485
David <davidtojo99@gmail.com>
2021-02-06 13:20:19 +0100

    Reducer de registro creado.

--- frontend/src/reducers/usuarioReducers.js
+++ frontend/src/reducers/usuarioReducers.js
@@ -1,4 +1,4 @@
-import { USUARIO_INICIOSESION_FAIL, USUARIO_INICIOSESION_REQUEST, USUARIO_INICIOSESION_SUCCESS } from "../constants/usuarioConstantes";
+import { USUARIO_INICIOSESION_FAIL, USUARIO_INICIOSESION_REQUEST, USUARIO_INICIOSESION_SUCCESS, USUARIO_REGISTRO_FAIL, USUARIO_REGISTRO_REQUEST, USUARIO_REGISTRO_SUCCESS } from "../constants/usuarioConstantes";
 
 function usuarioInicioSesionReducer(state = {}, action) {
     switch (action.type) {
@@ -13,4 +13,16 @@ function usuarioInicioSesionReducer(state = {}, action) {
     }
 }
 
-export { usuarioInicioSesionReducer }
+function usuarioRegistroReducer(state = {}, action) {
+    switch (action.type) {
+        case USUARIO_REGISTRO_REQUEST:
+            return { loading: true };
+        case USUARIO_REGISTRO_SUCCESS:
+            return { loading: false, usuarioInfo: action.payload };
+        case USUARIO_REGISTRO_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+export { usuarioInicioSesionReducer, usuarioRegistroReducer }
