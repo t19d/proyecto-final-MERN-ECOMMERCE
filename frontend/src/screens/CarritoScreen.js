073defc90985487989ef9977f9dd306203b50c62
David <davidtojo99@gmail.com>
2021-02-07 10:33:38 +0100

    Redirect a ventana de envío.

--- frontend/src/screens/CarritoScreen.js
+++ frontend/src/screens/CarritoScreen.js
@@ -24,7 +24,7 @@ function CarritoScreen(props) {
     }, []);
 
     const checkoutHandler = () => {
-        props.history.push("/iniciosesion?redirect=shipping");
+        props.history.push("/iniciosesion?redirect=envio");
     }
 
     return (
