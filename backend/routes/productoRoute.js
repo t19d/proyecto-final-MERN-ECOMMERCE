59c6861b07367e8cfe7ae16ce7a62a67cca3ad52
David <davidtojo99@gmail.com>
2021-02-07 09:16:35 +0100

    Añadido editar y eliminar.

--- backend/routes/productoRoute.js
+++ backend/routes/productoRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import Producto from '../models/productoModelo'
-import { getToken } from '../util';
+import Producto from '../models/productoModelo';
+import { isAdmin, isAuth } from '../util';
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.get("/", async (req, res) => {
     const productos = await Producto.find({});
     res.send(productos);
 });
+
+{/*TODO: isAuth e isAdmin son para que solo los administradores logeados puedan hacer las acciones de creación, edición y eliminación*/}
 
 router.post("/", async (req, res) => {
     const producto = new Producto({
@@ -30,6 +32,43 @@ router.post("/", async (req, res) => {
     } else {
         return res.status(500).send({ msg: 'Error creando producto.' });
     }
+});
+
+router.put("/:id", async (req, res) => {
+    const productoId = req.params.id;
+    const producto = await Producto.findOne({ _id: productoId });
+    if (producto) {
+        producto.nombre = req.body.nombre;
+        producto.miniatura = req.body.miniatura;
+        producto.precio = req.body.precio;
+        producto.imgDescripcion = req.body.imgDescripcion;
+        producto.descripcion = req.body.descripcion;
+        producto.imagenes = req.body.imagenes;
+        producto.categorias = req.body.categorias;
+        producto.cantidadStokXS = req.body.cantidadStokXS;
+        producto.cantidadStokS = req.body.cantidadStokS;
+        producto.cantidadStokM = req.body.cantidadStokM;
+        producto.cantidadStokL = req.body.cantidadStokL;
+        producto.cantidadStokXL = req.body.cantidadStokXL;
+
+        const updatedProducto = await producto.save();
+        if (updatedProducto) {
+            return res.status(200).send({ msg: 'Producto actualizando.', data: updatedProducto });
+        }
+    }
+    return res.status(500).send({ msg: 'Error actualizando producto.' });
+});
+
+
+
+router.delete("/:id", async (req, res) => {
+    const deletedProducto = await Producto.findById(req.params.id);
+    if (deletedProducto) {
+        await deletedProducto.remove();
+        res.send({ msg: 'Producto eliminado.' });
+    } else {
+        res.send({ msg: 'Error eliminando producto.' });
+    }        
 });
 
 export default router
