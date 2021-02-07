b06f6f9dea5e3ac30a29715b0486630edf95ca7d
David <davidtojo99@gmail.com>
2021-02-07 09:33:17 +0100

    Creado reducer eliminar, actualizados guardar/list

--- frontend/src/reducers/productoReducers.js
+++ frontend/src/reducers/productoReducers.js
@@ -1,9 +1,9 @@
-import { LISTA_PRODUCTOS_FAIL, LISTA_PRODUCTOS_REQUEST, LISTA_PRODUCTOS_SUCCESS, PRODUCTO_DETALLES_FAIL, PRODUCTO_DETALLES_REQUEST, PRODUCTO_DETALLES_SUCCESS, PRODUCTO_GUARDADO_FAIL, PRODUCTO_GUARDADO_REQUEST, PRODUCTO_GUARDADO_SUCCESS } from "../constants/productoConstantes";
+import { LISTA_PRODUCTOS_FAIL, LISTA_PRODUCTOS_REQUEST, LISTA_PRODUCTOS_SUCCESS, PRODUCTO_DETALLES_FAIL, PRODUCTO_DETALLES_REQUEST, PRODUCTO_DETALLES_SUCCESS, PRODUCTO_ELIMINADO_FAIL, PRODUCTO_ELIMINADO_REQUEST, PRODUCTO_ELIMINADO_SUCCESS, PRODUCTO_GUARDADO_FAIL, PRODUCTO_GUARDADO_REQUEST, PRODUCTO_GUARDADO_SUCCESS } from "../constants/productoConstantes";
 
 function listaProductosReducer(state = { productos: [] }, action) {
     switch (action.type) {
         case LISTA_PRODUCTOS_REQUEST:
-            return { loading: true };
+            return { loading: true, productos: [] };
         case LISTA_PRODUCTOS_SUCCESS:
             return { loading: false, productos: action.payload };
         case LISTA_PRODUCTOS_FAIL:
@@ -20,7 +20,7 @@ function productoGuardadoReducer(state = { producto: {} }, action) {
         case PRODUCTO_GUARDADO_SUCCESS:
             return { loading: false, success: true, producto: action.payload };
         case PRODUCTO_GUARDADO_FAIL:
-            return { loading: false, error: action.payload };
+            return { loading: false, success: false, error: action.payload };
         default:
             return state;
     }
@@ -39,4 +39,17 @@ function productoDetallesReducer(state = { producto: {} }, action) {
     }
 }
 
-export { listaProductosReducer, productoGuardadoReducer, productoDetallesReducer }
+function productoEliminadoReducer(state = { producto: {} }, action) {
+    switch (action.type) {
+        case PRODUCTO_ELIMINADO_REQUEST:
+            return { loading: true };
+        case PRODUCTO_ELIMINADO_SUCCESS:
+            return { loading: false, success: true, producto: action.payload };
+        case PRODUCTO_ELIMINADO_FAIL:
+            return { loading: false, success: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+
+export { listaProductosReducer, productoGuardadoReducer, productoDetallesReducer, productoEliminadoReducer }
