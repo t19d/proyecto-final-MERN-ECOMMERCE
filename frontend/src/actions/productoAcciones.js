1bab82d624d9200d022d6b3840b923114adc1d0f
David <davidtojo99@gmail.com>
2021-02-07 09:27:37 +0100

    Añadido eliminar y editado guardar.

--- frontend/src/actions/productoAcciones.js
+++ frontend/src/actions/productoAcciones.js
@@ -1,4 +1,4 @@
-import { LISTA_PRODUCTOS_FAIL, LISTA_PRODUCTOS_REQUEST, LISTA_PRODUCTOS_SUCCESS, PRODUCTO_DETALLES_FAIL, PRODUCTO_DETALLES_REQUEST, PRODUCTO_DETALLES_SUCCESS, PRODUCTO_GUARDADO_REQUEST, PRODUCTO_GUARDADO_SUCCESS, PRODUCTO_GUARDADO_FAIL } from "../constants/productoConstantes";
+import { LISTA_PRODUCTOS_FAIL, LISTA_PRODUCTOS_REQUEST, LISTA_PRODUCTOS_SUCCESS, PRODUCTO_DETALLES_FAIL, PRODUCTO_DETALLES_REQUEST, PRODUCTO_DETALLES_SUCCESS, PRODUCTO_GUARDADO_REQUEST, PRODUCTO_GUARDADO_SUCCESS, PRODUCTO_GUARDADO_FAIL, PRODUCTO_ELIMINADO_REQUEST, PRODUCTO_ELIMINADO_SUCCESS, PRODUCTO_ELIMINADO_FAIL } from "../constants/productoConstantes";
 import axios from 'axios';
 
 const hacerListaProductos = () => async (dispatch) => {
@@ -16,12 +16,21 @@ const guardarProducto = (producto) => async (dispatch, getState) => {
     try {
         dispatch({ type: PRODUCTO_GUARDADO_REQUEST, payload: producto });
         const { usuarioInicioSesion: { usuarioInfo } } = getState();
-        const { data } = await axios.post("/api/productos", producto, {
-            headers: {
-                'Authorization': 'Bearer' + usuarioInfo.token
-            }
-        });
-        dispatch({ type: PRODUCTO_GUARDADO_SUCCESS, payload: data });
+        if (!producto._id) {
+            const { data } = await axios.post("/api/productos", producto, {
+                headers: {
+                    'Authorization': 'Bearer' + usuarioInfo.token
+                }
+            });
+            dispatch({ type: PRODUCTO_GUARDADO_SUCCESS, payload: data });
+        } else {
+            const { data } = await axios.put("/api/productos/" + producto._id, producto, {
+                headers: {
+                    'Authorization': 'Bearer' + usuarioInfo.token
+                }
+            });
+            dispatch({ type: PRODUCTO_GUARDADO_SUCCESS, payload: data });
+        }
     } catch (error) {
         dispatch({ type: PRODUCTO_GUARDADO_FAIL, payload: error.message });
     }
@@ -38,4 +47,19 @@ const hacerProductoDetalles = (productoId) => async (dispatch) => {
     }
 }
 
-export { hacerListaProductos, hacerProductoDetalles, guardarProducto }
+const eliminarProducto = (productoId) => async (dispatch, getState) => {
+    try {
+        const { usuarioInicioSesion: { usuarioInfo } } = getState();
+        dispatch({ type: PRODUCTO_ELIMINADO_REQUEST, payload: productoId });
+        const { data } = await axios.delete("/api/productos/" + productoId, {
+            headers: {
+                'Authorization': 'Bearer' + usuarioInfo.token
+            }
+        });
+        dispatch({ type: PRODUCTO_ELIMINADO_SUCCESS, payload: data, success: true });
+    } catch (error) {
+        dispatch({ type: PRODUCTO_ELIMINADO_FAIL, payload: error.message });
+    }
+}
+
+export { hacerListaProductos, hacerProductoDetalles, guardarProducto, eliminarProducto }
