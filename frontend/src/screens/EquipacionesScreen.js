eabe1961598b5f4c7ad71c2bc99e6beca5533270
David <davidtojo99@gmail.com>
2021-02-10 20:42:26 +0100

    Mostrar solo las equipaciones.

--- frontend/src/screens/EquipacionesScreen.js
+++ frontend/src/screens/EquipacionesScreen.js
@@ -9,6 +9,17 @@ function EquipacionesScreen(props) {
     const { productos, loading, error } = listaProductos;
     const dispatch = useDispatch();
 
+    const isEquipacion = (categorias) => {
+        var respuesta = false;
+        console.log(categorias)
+        categorias.forEach(element => {
+            if (element.toLowerCase() === "equipacion") {
+                respuesta = true;
+            }
+        });
+        return respuesta;
+    }
+
     useEffect(() => {
         dispatch(hacerListaProductos());
         return () => {
@@ -19,29 +30,25 @@ function EquipacionesScreen(props) {
     return (
         <section>
             <img src="/images/recursos_web/cabecera_camiseta_blanca_grande.png" width="100%" alt="Cabecera camiseta blanca" />
+            {loading && <div className="d-flex justify-content-center"><img src="/images/recursos_web/loading.gif" alt="Cargando" /></div>}
+            {error && <div>{error}</div>}
             <div className="container">
-                {
-                    loading ? <div>Cargando...</div> :
-                        error ? <div>{error}</div> :
-                            (
-                                <div className="row listaProductos">
-                                    {
-                                        productos.map(
-                                            producto =>
-                                            (<Link to={'/productos/' + producto._id} className="col-sm itemProductos" key={producto._id}>
-                                                <div className="card text-center">
-                                                    <div className="card-body">
-                                                        <img src={producto.miniatura} className="card-img imagenItem" alt={producto.imgDescripcion} />
-                                                        <h5 className="card-title">{producto.nombre}</h5>
-                                                        <h5 className="card-title precioItem">{producto.precio} €</h5>
-                                                    </div>
-                                                </div>
-                                            </Link>)
-                                        )
-                                    }
+                <div className="row listaProductos">
+                    {
+                        productos.map(
+                            producto =>
+                            isEquipacion(producto.categorias) && (<Link to={'/productos/' + producto._id} className="col-sm itemProductos" key={producto._id}>
+                                <div className="card text-center">
+                                    <div className="card-body">
+                                        <img src={producto.miniatura} className="card-img imagenItem" alt={producto.imgDescripcion} />
+                                        <h5 className="card-title">{producto.nombre}</h5>
+                                        <h5 className="card-title precioItem">{producto.precio} €</h5>
+                                    </div>
                                 </div>
-                            )
-                }
+                            </Link>)
+                        )
+                    }
+                </div>
             </div>
         </section>);
 }
