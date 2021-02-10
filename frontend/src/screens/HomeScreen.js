2efe90c299d92c5b385c99c2f52313fce3819068
David <davidtojo99@gmail.com>
2021-02-10 20:39:35 +0100

    Función para mostrar solo elementos con categoría portada.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { hacerListaProductos } from '../actions/productoAcciones'
 
 function HomeScreen(props) {
-    /*const equipacionLocal = data.productos.find(p => p._id === '1-equipacion-adulto-20-21-club-pastoriza-balompie');
-    const equipacionVisitante = data.productos.find(p => p._id === '2-equipacion-adulto-20-21-club-pastoriza-balompie');*/
+
+    const listaProductos = useSelector(state => state.listaProductos);
+    const { productos, loading, error } = listaProductos;
+    const dispatch = useDispatch();
+
+    const isPortada = (categorias) => {
+        var respuesta = false;
+        console.log(categorias)
+        categorias.forEach(element => {
+            if (element.toLowerCase() === "portada") {
+                respuesta = true;
+            }
+        });
+        return respuesta;
+    }
+
+    useEffect(() => {
+        dispatch(hacerListaProductos());
+        return () => {
+            //
+        }
+    }, []);
     return (
         <section>
-            <a href="/equipaciones"><img src="/images/recursos_web/Cabecera_camiseta_negra_grande.png" width="100%" alt="Cabecera camiseta negra"/></a>
+            <a href="/equipaciones"><img src="/images/recursos_web/Cabecera_camiseta_negra_grande.png" width="100%" alt="Cabecera camiseta negra" /></a>
             <div className="container">
                 <h1 className="text-center text-uppercase font-weight-bold">Equipaciones Oficiales</h1>
                 <div className="row listaProductos">
-                    <Link to={'/productos/' + equipacionLocal._id} className="col-sm itemProductos">
+                    {
+                        productos.map(
+                            producto =>
+                                isPortada(producto.categorias) && (<Link to={'/productos/' + producto._id} className="col-sm itemProductos" key={producto._id}>
+                                    <div className="card text-center">
+                                        <div className="card-body">
+                                            <img src={producto.miniatura} className="card-img imagenItem" alt={producto.imgDescripcion} />
+                                            <h5 className="card-title">{producto.nombre}</h5>
+                                            <h5 className="card-title precioItem">{producto.precio} €</h5>
+                                        </div>
+                                    </div>
+                                </Link>)
+                        )
+                    }
+                    {/*<Link to={'/productos/' + equipacionLocal._id} className="col-sm itemProductos">
                         <div className="card text-center">
                             <div className="card-body">
                                 <img src={equipacionLocal.miniatura} className="card-img imagenItem" alt={equipacionLocal.imgDescripcion} />
@@ -27,7 +63,7 @@ function HomeScreen(props) {
                                 <h5 className="card-title precioItem">{equipacionVisitante.precio} €</h5>
                             </div>
                         </div>
-                    </Link>
+                    </Link>*/}
                 </div>
             </div>
         </section>
