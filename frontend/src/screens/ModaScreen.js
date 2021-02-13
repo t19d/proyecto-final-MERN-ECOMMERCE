1d3c860cf9c4f6821b44f7db391691d9b691fbc5
David <davidtojo99@gmail.com>
2021-02-13 08:48:56 +0100

    Creado orderBy.

--- frontend/src/screens/ModaScreen.js
+++ frontend/src/screens/ModaScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { hacerListaProductos } from '../actions/productoAcciones'
@@ -8,6 +8,8 @@ function ModaScreen(props) {
     const listaProductos = useSelector(state => state.listaProductos);
     const { productos, loading, error } = listaProductos;
     const dispatch = useDispatch();
+    const [orden, setOrden] = useState('mayMen');
+    const listaOrdenadaProductos = productos;
 
     const isRopa = (categorias) => {
         var respuesta = false;
@@ -17,6 +19,15 @@ function ModaScreen(props) {
             }
         });
         return respuesta;
+    }
+
+    const ordenarPor = (order) => {
+        switch (order) {
+            case "menMey":
+                return listaOrdenadaProductos.sort((productoA, productoB) => productoA.precio - productoB.precio);
+            case "mayMen":
+                return listaOrdenadaProductos.sort((productoA, productoB) => productoA.precio - productoB.precio).reverse();
+        }
     }
 
     useEffect(() => {
@@ -31,13 +42,19 @@ function ModaScreen(props) {
             {loading && <div className="d-flex justify-content-center"><img src="/images/recursos_web/loading.gif" alt="Cargando" /></div>}
             {error && <div>{error}</div>}
             <div className="container">
+                <div className="ordenarPorListaProductos">
+                    <select id="ordenarPorProductos" value={orden} onChange={(e) => setOrden(e.target.value)}>
+                        <option value="mayMen">Ordenar por precio: mayor a menor</option>
+                        <option value="menMey">Ordenar por precio: menor a mayor</option>
+                    </select>
+                </div>
                 <ul className="row listaProductos">
                     {
-                        productos.map(
+                        ordenarPor(orden).map(
                             producto =>
                                 isRopa(producto.categorias) && (
-                                    <li class="col-md-4 col-sm-6 col-xs-12">
-                                        <Link to={'/productos/' + producto._id} className="col-sm itemProductos" key={producto._id}>
+                                    <li className="col-md-4 col-sm-6 col-xs-12" key={producto._id}>
+                                        <Link to={'/productos/' + producto._id} className="col-sm itemProductos">
                                             <div className="card text-center">
                                                 <div className="card-body">
                                                     <img src={producto.miniatura} className="card-img imagenItem" alt={producto.imgDescripcion} />
