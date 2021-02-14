1b72353e0d9b2528c669975c4325a8d79601e0db
David <davidtojo99@gmail.com>
2021-02-14 09:44:30 +0100

    Mejor control sobre el formulario.

--- frontend/src/screens/PagoScreen.js
+++ frontend/src/screens/PagoScreen.js
@@ -1,11 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { guardarPago } from '../actions/carritoAcciones';
 import CheckoutPasos from '../components/CheckoutPasos';
 
 function PagoScreen(props) {
-    const [metodoPago, setMetodoPago] = useState('');
+    const usuarioInicioSesion = useSelector(state => state.usuarioInicioSesion);
+    const { usuarioInfo } = usuarioInicioSesion;
+
+    // Si no ha iniciado sesión, se envía
+    if (!usuarioInfo) {
+        props.history.push('/iniciosesion');
+    }
+
+    const carrito = useSelector(state => state.carrito);
+    const { carritoItems, pago, envio } = carrito;
+
+    // Si el carrito está vacío, se envía
+    if (carritoItems.length === 0) {
+        props.history.push('/carrito');
+    }
+
+    // Si el envío está vacío, se envía
+    if (!envio) {
+        props.history.push('/envio');
+    }
+
+    const [metodoPago, setMetodoPago] = useState((pago) ? pago.metodoPago : 'PayPal');
 
     const dispatch = useDispatch();
 
@@ -21,8 +41,8 @@ function PagoScreen(props) {
             <div className="text-center container-sm" onSubmit={submitHandler}>
                 <h1 className="text-center tituloPagina">Pago</h1>
                 <form className="formRegistro">
-                    <input type="radio" name="metodoPago" id="metodoPago" value="Paypal" onChange={(e) => setMetodoPago(e.target.value)}></input>
-                    <label htmlFor="metodoPago">Paypal</label>
+                    <input type="radio" name="metodoPago" id="metodoPago" value="PayPal" checked onChange={(e) => setMetodoPago(e.target.value)}></input>
+                    <label htmlFor="metodoPago">PayPal</label>
 
                     <button className="btn btn-lg btn-primary btn-block" type="submit">Continuar</button>
                 </form>
