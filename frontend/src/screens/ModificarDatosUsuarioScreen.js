1f09a77ff7e306f70db2b5e09bfd33c263fb6cc8
David <davidtojo99@gmail.com>
2021-02-16 08:59:42 +0100

    Hacer efectivos los cambios en bbdd y mostrarlos.

--- frontend/src/screens/ModificarDatosUsuarioScreen.js
+++ frontend/src/screens/ModificarDatosUsuarioScreen.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { actualizarUsuario, detallesUsuario } from '../actions/usuarioAcciones';
+import { actualizarUsuario, cerrarSesion, detallesUsuario } from '../actions/usuarioAcciones';
 import { USUARIO_MODIFICARDATOS_RESET } from '../constants/usuarioConstantes';
 
 function ModificarDatosUsuarioScreen(props) {
 
+    const usuarioInicioSesion = useSelector(state => state.usuarioInicioSesion);
+    const { usuarioInfo } = usuarioInicioSesion;
+
     const usuarioDetalles = useSelector((state) => state.usuarioDetalles);
     const { loading, error, usuario } = usuarioDetalles;
 
-    const usuarioId = usuario ? usuario._id : "";
+    const usuarioId = usuario ? usuario._id : usuarioInfo._id;
     const [nombre, setNombre] = useState(usuario ? usuario.nombre : "");
     const [email, setEmail] = useState(usuario ? usuario.email : "");
     const [isAdmin, setIsAdmin] = useState(false);
@@ -19,7 +22,9 @@ function ModificarDatosUsuarioScreen(props) {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        dispatch(actualizarUsuario(usuario));
+        dispatch(actualizarUsuario({
+            _id: usuarioId, isAdmin: isAdmin, nombre: nombre, email: email, password: usuario.password
+        }));
     };
 
     const volverPerfil = () => {
@@ -30,7 +35,14 @@ function ModificarDatosUsuarioScreen(props) {
     useEffect(() => {
         if (successActualizacion) {
             dispatch({ type: USUARIO_MODIFICARDATOS_RESET });
-            volverPerfil();
+            if (!loadingActualizacion) {
+                dispatch(detallesUsuario(usuarioId));
+                if (usuarioInicioSesion.email === usuario.email) {
+                    volverPerfil();
+                } else {
+                    dispatch(cerrarSesion());
+                }
+            }
         }
         if (!usuario) {
             dispatch(detallesUsuario(usuarioId));
@@ -39,22 +51,24 @@ function ModificarDatosUsuarioScreen(props) {
             setEmail(usuario.email);
             setIsAdmin(usuario.isAdmin);
         }
-    }, [dispatch, props.history, successActualizacion, usuario, usuarioId, loading]);
+    }, [dispatch, props.history, successActualizacion, usuario, usuarioId, loadingActualizacion]);
 
     return (
         <div className="text-center container-sm" onSubmit={submitHandler}>
             <h1 className="text-center tituloPagina">Editar datos usuario</h1>
             {loading && <div className="d-flex justify-content-center"><img src="/images/recursos_web/loading.gif" alt="Cargando" /></div>}
             {error && <div>{error}</div>}
+            {loadingActualizacion && <div className="d-flex justify-content-center"><img src="/images/recursos_web/loading.gif" alt="Cargando" /></div>}
+            {errorActualizacion && <div>{errorActualizacion}</div>}
             <form className="formRegistro">
-                <input id="inputNombre" className="form-control" type="text" value={nombre} placeholder="Nombre" required autoFocus="" onChange={(e) => setNombre(e.target.value)} />
-                <input id="inputEmail" className="form-control" type="email" value={email} placeholder="Email" required autoFocus="" onChange={(e) => setEmail(e.target.value)} />
+                <input id="inputNombre" className="form-control" type="text" value={nombre} placeholder="Nombre" required autoFocus onChange={(e) => setNombre(e.target.value)} />
+                <input id="inputEmail" className="form-control" type="email" value={email} placeholder="Email" required onChange={(e) => setEmail(e.target.value)} />
                 <div className="row">
                     <div className="col-md-6 col-sm-6 col-xs-12">
-                        <button className="btn btn-lg btn-danger btn-block" type="submit">Cancelar</button>
+                        <button className="btn btn-lg btn-danger btn-block" onClick={volverPerfil}>Cancelar</button>
                     </div>
                     <div className="col-md-6 col-sm-6 col-xs-12">
-                        <button className="btn btn-lg btn-primary btn-block" onClick={volverPerfil} >Confirmar</button>
+                        <button className="btn btn-lg btn-primary btn-block" type="submit">Confirmar</button>
                     </div>
                 </div>
             </form>
