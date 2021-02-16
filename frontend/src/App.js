b32a2758e979f2e19d1a5c603fc063e8a2f53079
David <davidtojo99@gmail.com>
2021-02-16 08:19:30 +0100

    Añadida ventana modificar datos.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,6 +21,7 @@ import PerfilScreen from './screens/PerfilScreen';
 import { cerrarSesion } from './actions/usuarioAcciones';
 import ListaPedidosScreen from './screens/ListaPedidosScreen';
 import PedidoDetalleScreen from './screens/PedidoDetalleScreen';
+import ModificarDatosUsuarioScreen from './screens/ModificarDatosUsuarioScreen';
 
 function App() {
 
@@ -191,6 +192,7 @@ function App() {
         <Route path="/accesorios" component={AccesoriosScreen} />
         <Route path="/outlet" component={OutletScreen} />
         <Route path="/perfil" component={PerfilScreen} />
+        <Route path="/editarusuario" component={ModificarDatosUsuarioScreen} />
         <Route path="/pedidos" component={ListaPedidosScreen} />
         <Route path="/mispedidos/:id" component={PedidoDetalleScreen} />
       </div>
