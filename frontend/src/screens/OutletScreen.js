34859ff8506e6c44a2a83b73e361b10e5aae56ff
David <davidtojo99@gmail.com>
2021-02-27 17:04:17 +0100

    Quitada zona filtro.

--- frontend/src/screens/OutletScreen.js
+++ frontend/src/screens/OutletScreen.js
@@ -39,41 +39,34 @@ function OutletScreen(props) {
                 <h1 className="text-center tituloPagina">Outlet</h1>
                 {loading && <div className="d-flex justify-content-center"><img src="/images/recursos_web/loading.gif" alt="Cargando" /></div>}
                 {error && <div>{error}</div>}
-                <div className="row">
-                    <div id="contenedorFiltros" className="col-sm-3">
-                        <div>Filtro</div>
-                    </div>
-                    <div className="col-sm-9">
-                        <div className="ordenarPorListaProductos">
-                            <select id="ordenarPorProductos" value={orden} onChange={(e) => setOrden(e.target.value)}>
-                                <option value="mayMen">Ordenar por precio: mayor a menor</option>
-                                <option value="menMey">Ordenar por precio: menor a mayor</option>
-                            </select>
-                        </div>
-                        <ul className="row listaProductos">
-                            {
-                                ordenarPor(orden).map(
-                                    producto =>
-                                        /*isOutlet(producto.categorias) &&*/ (
-                                            <li className="col-md-4 col-sm-6 col-xs-12" key={producto._id}>
-                                                <Link to={'/productos/' + producto._id} className="col-sm itemProductos">
-                                                    <div className="card text-center">
-                                                        <div className="card-body">
-                                                            {(producto.precioOferta < producto.precio) && <h4 className="descuentoPrecioOferta">{(((producto.precioOferta * (-100)) / producto.precio) + 100).toFixed(0)}%</h4>}
-                                                            <img src={producto.miniatura} className="card-img imagenItem" alt={producto.imgDescripcion} />
-                                                            <h5 className="card-title">{producto.nombre}</h5>
-                                                            {(producto.precioOferta < producto.precio) ? <h5 className="card-title precioItem">{producto.precioOferta}€ <span className="precioAntiguoOfertaLista">{producto.precio}€</span></h5> :
-                                                                <h5 className="card-title precioItem">{producto.precio} €</h5>}
-                                                        </div>
-                                                    </div>
-                                                </Link>
-                                            </li>
-                                        )
-                                )
-                            }
-                        </ul>
-                    </div>
+                <div className="ordenarPorListaProductos">
+                    <select id="ordenarPorProductos" value={orden} onChange={(e) => setOrden(e.target.value)}>
+                        <option value="mayMen">Ordenar por precio: mayor a menor</option>
+                        <option value="menMey">Ordenar por precio: menor a mayor</option>
+                    </select>
                 </div>
+                <ul className="row listaProductos">
+                    {
+                        ordenarPor(orden).map(
+                            producto =>
+                                        /*isOutlet(producto.categorias) &&*/(
+                                <li className="col-md-4 col-sm-6 col-xs-12" key={producto._id}>
+                                    <Link to={'/productos/' + producto._id} className="col-sm itemProductos">
+                                        <div className="card text-center">
+                                            <div className="card-body">
+                                                {(producto.precioOferta < producto.precio) && <h4 className="descuentoPrecioOferta">{(((producto.precioOferta * (-100)) / producto.precio) + 100).toFixed(0)}%</h4>}
+                                                <img src={producto.miniatura} className="card-img imagenItem" alt={producto.imgDescripcion} />
+                                                <h5 className="card-title">{producto.nombre}</h5>
+                                                {(producto.precioOferta < producto.precio) ? <h5 className="card-title precioItem">{producto.precioOferta}€ <span className="precioAntiguoOfertaLista">{producto.precio}€</span></h5> :
+                                                    <h5 className="card-title precioItem">{producto.precio} €</h5>}
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </li>
+                            )
+                        )
+                    }
+                </ul>
             </div>
         </section>);
 }
