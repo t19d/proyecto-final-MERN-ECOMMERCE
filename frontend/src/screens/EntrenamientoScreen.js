b28e3db9d4b3edfe6e738e01154e01af1b3e453b
David <davidtojo99@gmail.com>
2021-02-27 17:06:15 +0100

    Filtro añadido.

--- frontend/src/screens/EntrenamientoScreen.js
+++ frontend/src/screens/EntrenamientoScreen.js
@@ -9,6 +9,56 @@ function EntrenamientoScreen(props) {
     const { productos, loading, error } = listaProductos;
     const dispatch = useDispatch();
     const [orden, setOrden] = useState('mayMen');
+    const [filtros, setFiltros] = useState([]);
+
+    const filtrarProductos = () => {
+        return (getProductosEntrenamiento(productos).filter(itemProducto => {
+            return filtros.every((itemFiltro) => {
+                switch (itemFiltro) {
+                    case "XS":
+                        if (itemProducto.cantidadStockXS > 0) {
+                            return itemProducto;
+                        }
+                        break;
+                    case "S":
+                        if (itemProducto.cantidadStockS > 0) {
+                            return itemProducto;
+                        }
+                        break;
+                    case "M":
+                        if (itemProducto.cantidadStockM > 0) {
+                            return itemProducto;
+                        }
+                        break;
+                    case "L":
+                        if (itemProducto.cantidadStockL > 0) {
+                            return itemProducto;
+                        }
+                        break;
+                    case "XL":
+                        if (itemProducto.cantidadStockXL > 0) {
+                            return itemProducto;
+                        }
+                        break;
+                    case "19_20":
+                        return itemProducto.categorias.includes('19_20');
+                    case "20_21":
+                        return itemProducto.categorias.includes('20_21');
+                    case "chandal":
+                        return itemProducto.categorias.includes('chandal');
+                }
+            })
+        }));
+    }
+
+    const anhadirFiltro = (itemFiltro) => {
+        // Comprobar si ya está puesto el filtro
+        if (filtros.includes(itemFiltro) || filtros.empty) {
+            setFiltros(filtros.filter(f => f != itemFiltro));
+        } else {
+            setFiltros(filtros.concat(itemFiltro));
+        }
+    }
 
     const getProductosEntrenamiento = (productos) => {
         // Mostrar solo los de la categoría entrenamiento
@@ -20,9 +70,9 @@ function EntrenamientoScreen(props) {
     const ordenarPor = (order) => {
         switch (order) {
             case "menMey":
-                return getProductosEntrenamiento(productos).sort((productoA, productoB) => productoA.precioOferta - productoB.precioOferta);
+                return filtrarProductos().sort((productoA, productoB) => productoA.precioOferta - productoB.precioOferta);
             case "mayMen":
-                return getProductosEntrenamiento(productos).sort((productoA, productoB) => productoA.precioOferta - productoB.precioOferta).reverse();
+                return filtrarProductos().sort((productoA, productoB) => productoA.precioOferta - productoB.precioOferta).reverse();
         }
     }
 
@@ -41,7 +91,43 @@ function EntrenamientoScreen(props) {
                 {error && <div>{error}</div>}
                 <div className="row">
                     <div id="contenedorFiltros" className="col-sm-3">
-                        <div>Filtro</div>
+                        <h3>Filtrar por temporada</h3>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="19_20" id="filtrarPorProducto1920" onChange={(e) => anhadirFiltro(e.target.value)} />
+                            <label className="form-check-label" htmlFor="filtrarPorProducto1920">19/20</label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="20_21" id="filtrarPorProducto2021" onChange={(e) => anhadirFiltro(e.target.value)} />
+                            <label className="form-check-label" htmlFor="filtrarPorProducto2021">20/21</label>
+                        </div>
+                        <h3>Filtrar por tipo de producto</h3>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="chandal" id="filtrarPorProductoMangaChandal" onChange={(e) => anhadirFiltro(e.target.value)} />
+                            <label className="form-check-label" htmlFor="filtrarPorProductoChandal">Chándal</label>
+                        </div>
+
+
+                        <h3>Filtrar por talla</h3>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="XS" id="filtrarPorTallaXS" onChange={(e) => anhadirFiltro(e.target.value)} />
+                            <label className="form-check-label" htmlFor="filtrarPorTallaXS">XS</label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="S" id="filtrarPorTallaS" onChange={(e) => anhadirFiltro(e.target.value)} />
+                            <label className="form-check-label" htmlFor="filtrarPorTallaS">S</label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="M" id="filtrarPorTallaM" onChange={(e) => anhadirFiltro(e.target.value)} />
+                            <label className="form-check-label" htmlFor="filtrarPorTallaM">M</label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="L" id="filtrarPorTallaL" onChange={(e) => anhadirFiltro(e.target.value)} />
+                            <label className="form-check-label" htmlFor="filtrarPorTallaL">L</label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="XL" id="filtrarPorTallaXL" onChange={(e) => anhadirFiltro(e.target.value)} />
+                            <label className="form-check-label" htmlFor="filtrarPorTallaXL">XL</label>
+                        </div>
                     </div>
                     <div className="col-sm-9">
                         <div className="ordenarPorListaProductos">
@@ -50,10 +136,11 @@ function EntrenamientoScreen(props) {
                                 <option value="menMey">Ordenar por precio: menor a mayor</option>
                             </select>
                         </div>
-                        <ul className="row listaProductos">
-                            {
-                                ordenarPor(orden).map(
-                                    producto =>
+                        {ordenarPor(orden).length > 0 ?
+                            <ul className="row listaProductos">
+                                {
+                                    ordenarPor(orden).map(
+                                        producto =>
                                         /*isEntrenamiento(producto.categorias) && */(
                                             <li className="col-md-4 col-sm-6 col-xs-12" key={producto._id}>
                                                 <Link to={'/productos/' + producto._id} className="col-sm itemProductos">
@@ -69,9 +156,19 @@ function EntrenamientoScreen(props) {
                                                 </Link>
                                             </li>
                                         )
-                                )
-                            }
-                        </ul>
+                                    )
+                                }
+                            </ul> : <h3 className="text-center text-danger">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height=".75em" fill="currentColor" className="bi bi-emoji-frown" viewBox="0 0 16 18">
+                                        <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path fillRule="evenodd" d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683z" />
+                                        <path d="M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z" />
+                                    </svg>
+                                </span>
+                                <span>No hay resultados</span>
+                            </h3>}
+
                     </div>
                 </div>
             </div>
