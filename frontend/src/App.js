51f666c9b41a41064d0d52ef63bd751e1e4ed719
David <davidtojo99@gmail.com>
2021-02-27 17:53:45 +0100

    Responsive menú añadido.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -139,7 +139,7 @@ function App() {
             </div>
             <nav className="navbar navbar-expand-lg justify-content-end">
               <div id="header_inferior_derecha">
-                <button className=" navbar-toggler navbar-dark bg-dark" type="button" data-toggle="collapse"
+                <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse"
                   data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
                   aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
@@ -180,7 +180,7 @@ function App() {
                         <span>OUTLET</span>
                       </Link>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item" >
                       <Link to="/carrito" className="nav-link">
                         <span>
                           <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 22">
@@ -193,6 +193,38 @@ function App() {
                 </div>
               </div>
             </nav>
+            <div id="contenedorPequeImagen">
+              <Link to="/">
+                <img id="header_escudo_peque" src="/images/escudos/vector_escudo_clubpastorizabalompie.svg" alt="Icono inicio" />
+              </Link>
+            </div>
+            <div id="contenedorPequeUsuarioCarrito">
+              <ul>
+                <li>
+                  <Link to={usuarioInfo ? "/perfil" : "/iniciosesion"} className="">
+                    <span>
+                      <svg width="2.5em" height="2.5em" viewBox="0 0 16 16" className="bi bi-person-circle"
+                        fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z" />
+                        <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                        <path fillRule="evenodd"
+                          d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z" />
+                      </svg>
+                    </span>
+                  </Link>
+                </li>
+                <li className="nav-item" >
+                  <Link to="/carrito" className="">
+                    <span>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="2.5em" height="2.5em" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                      </svg>
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </header>
