a6d4647d6972954ad35e079e553c8524166d3198
David <davidtojo99@gmail.com>
2021-02-28 08:36:29 +0100

    Diseño responsive manage productos.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -200,8 +200,8 @@ function App() {
             </div>
             <div id="contenedorPequeUsuarioCarrito">
               <ul>
-                {usuarioInfo &&
-                  <li>
+                <li>
+                  {usuarioInfo &&
                     <Link to="#cerrarsesion" className="-item" onClick={cerrarSesionHandler}>
                       <span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="2.5em" height="2.5em" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
@@ -209,8 +209,8 @@ function App() {
                         </svg>
                       </span>
                     </Link>
-                  </li>
-                }
+                  }
+                </li>
                 <li>
                   <Link to={usuarioInfo ? "/perfil" : "/iniciosesion"} className="">
                     <span>
@@ -224,6 +224,19 @@ function App() {
                       </svg>
                     </span>
                   </Link>
+                </li>
+              </ul>
+              <ul>
+                <li className="nav-item" >
+                  {usuarioInfo && usuarioInfo.isAdmin === true &&
+                    <Link to="/manageproductos" className="">
+                      <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="2.5em" height="2.5em" fill="currentColor" class="bi bi-sliders" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z" />
+                        </svg>
+                      </span>
+                    </Link>
+                  }
                 </li>
                 <li className="nav-item" >
                   <Link to="/carrito" className="">
